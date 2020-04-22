c1f0e1f08d57a99eed102ca756963d560a40b46f
buddhagandhi <buddha.gandhi8@gmail.com>
2020-04-22 15:45:48 +0530

    commit java practice april 22nd

---
+++ src/StringBuilderClass.java
@@ -0,0 +1,7 @@
+public class StringBuilderClass {
+    public static void main(String args []) {
+    // StringBuilder when you’re dealing with larger strings or modifying the contents of a string often.It is mutable
+    // In contrast to the class String, the class StringBuilder uses a non–final char array to store its value.
+
+        }
+    }
